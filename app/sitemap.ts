9910c83a06e80aa4769ac8660d2de0e360a8c56f
anandtejaswi <anand.tejaswi05@gmail.com>
2026-06-19 01:54:34 +0530

    About page added to sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1.0,
         },
         {
+            url: `${siteConfig.url}/about`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
             url: `${siteConfig.url}/certifications`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
