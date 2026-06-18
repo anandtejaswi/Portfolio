779e6bb9ada965c42beefe559d447f356c85aab0
anandtejaswi <anand.tejaswi05@gmail.com>
2026-06-19 02:20:09 +0530

    Security Imporvements made

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,56 @@
 import type { NextConfig } from 'next';
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https:;
+  font-src 'self' data:;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  frame-src 'self' https://tryhackme.com;
+  upgrade-insecure-requests;
+`;
+
+const securityHeaders = [
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin'
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+        key: 'Cross-Origin-Opener-Policy',
+        value: 'same-origin'
+    }
+];
+
 const nextConfig: NextConfig = {
     transpilePackages: ['next-mdx-remote'],
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
