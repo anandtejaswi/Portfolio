9e2b28b35c88aa27dc47570a547402c254836b09
anandtejaswi <anand.tejaswi05@gmail.com>
2026-06-18 17:29:38 +0530

    Commit

--- nextjs-portfolio/components/grid/widgets/eligify.tsx
+++ nextjs-portfolio/components/grid/widgets/eligify.tsx
@@ -1,4 +1,4 @@
-import projectImage from '@/public/images/eligify1.png';
+import projectImage from '@/public/images/Eligify-card.jpg';
 import Card from '../../ui/card';
 import Image from 'next/image';
 import Anchor from '../../ui/anchor';
